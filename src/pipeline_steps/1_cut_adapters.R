6d05ba959fec7fee2a4e128774cf2097dd4ec3db
Alex Y <github.alex.ai@gmail.com>
2021-02-11 14:37:43 +0300

    path correction

--- src/pipeline_steps/1_cut_adapters.R
+++ src/pipeline_steps/1_cut_adapters.R
@@ -8,11 +8,12 @@ print(no_primers_path)
 # run bash script
 # https://stackoverflow.com/questions/11395217/run-a-bash-script-from-an-r-script
 # form a bash string (make sure it has execution permissions)
-x1 <- file.path(project_path, 'src/pipeline_building_blocks/1_cut_adapters.sh')   # script to run
+x1 <- file.path(project_path, 'src/pipeline_steps/1_cut_adapters.sh')   # script to run
 x <- paste(x1, raw_path)
 cat(x)  # sanity check
 
 
 # run bash command
+# NOTE: first path is a path to Python in conda env, the second is to cutadapt itself
 system("/Users/alex/anaconda3/envs/py3.6_twins/bin/python /Users/alex/anaconda3/envs/py3.6_twins/bin/cutadapt --version")
 system(x)
